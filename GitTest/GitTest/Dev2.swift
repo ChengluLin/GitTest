cb2df346123060114aa3705bba8aa84f8b33965e
Kevin <chenglu@efriend.com.tw>
2022-11-16 16:18:44 +0800

    no message

--- GitTest/GitTest/Dev2.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Dev2.swift
-//  GitTest
-//
-//  Created by ChengLu on 2022/11/15.
-//
-
-import Foundation
--- GitTest/GitTest/Dev7.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Dev7.swift
-//  GitTest
-//
-//  Created by ChengLu on 2022/11/16.
-//
-
-import Foundation
-// dev7 content
